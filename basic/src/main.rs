5d8aa94a29b86d2c48ba3f5732339a0fdc9eb31d
sinhu-jung <tlsgn26@naver.com>
2025-11-07 17:49:49 +0900

    불변성 변수와 가변성 변수

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // 변수와 가변성
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    // Rust에서 기본적으로 변수는 불변(immutable)
+    // x = 6;
+    // println!("The value of x is: {}", x);
+
+    let mut y = 10; // 가변(mutable) 변수 선언
+    println!("The value of y is: {}", y);
+    y = 15; // 가변 변수는 값을 변경할 수 있음
+    println!("The value of y is: {}", y);
+    
 }
